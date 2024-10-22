a0303d87eef496c54c8eca0b20a9d51a2a1b2cce
husseinkamal <hussain@aqwas.sa>
2024-10-22 14:25:36 +0300

    Create transformation for courses viewpage in HomeFragment.kt

--- app/src/main/java/com/hussein/mazaady/ui/home/CategoryActivity.kt
+++ app/src/main/java/com/hussein/mazaady/ui/home/CategoryActivity.kt
@@ -1,5 +1,6 @@
 package com.hussein.mazaady.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -46,6 +47,10 @@ class CategoryActivity : AppCompatActivity(), OnCategoryListener, OnSubCategoryL
             //Show Main Categories
             val bottomSheetDialogFragment = CategoryBottomSheetDialog.newInstance(this@CategoryActivity)
             bottomSheetDialogFragment.show(supportFragmentManager, CategoryBottomSheetDialog.TAG)
+        }
+
+        binding.lyTableContainer.gotoHomeBtn.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 
